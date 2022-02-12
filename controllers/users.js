ed994c5172f00148d82331868b7df3b7c00f4547
Veronika Malyshkova <malyshkovav@mac-profi.ru>
2022-02-13 02:12:35 +0300

    fix: changed cookie sattings

--- controllers/users.js
+++ controllers/users.js
@@ -57,7 +57,7 @@ const login = (req, res, next) => {
       res.cookie('jwt', token, {
         maxAge: 3600000 * 24 * 7,
         httpOnly: true,
-        sameSite: true,
+        sameSite: 'none', // изменила с true
       }).send({ token });
     })
     .catch(() => {
